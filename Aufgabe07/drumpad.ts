394df9b1b0641ad4894b1ec02ad2a1a68095e0ea
Woschny <92034289+Woschny@users.noreply.github.com>
2021-11-26 14:36:29 +0100

    Unnötige Simicolons entfernt

--- Aufgabe07/drumpad.ts
+++ Aufgabe07/drumpad.ts
@@ -8,15 +8,15 @@ var order:string [] = ['assets/snare.mp3','assets/kick.mp3','assets/hihat.mp3','
 var key:number = 0;
 
 //7.1 Weitergabe von Array Variabel an Main Funktion beim Click auf Knopf              
-document.querySelector(".knopf0").addEventListener("click", function () {playSample(link[0]);});
-document.querySelector(".knopf1").addEventListener("click", function () {playSample(link[1]);});
-document.querySelector(".knopf2").addEventListener("click", function () {playSample(link[2]);});
-document.querySelector(".knopf3").addEventListener("click", function () {playSample(link[3]);});
-document.querySelector(".knopf4").addEventListener("click", function () {playSample(link[4]);});
-document.querySelector(".knopf5").addEventListener("click", function () {playSample(link[5]);});
-document.querySelector(".knopf6").addEventListener("click", function () {playSample(link[6]);});
-document.querySelector(".knopf7").addEventListener("click", function () {playSample(link[7]);});
-document.querySelector(".knopf8").addEventListener("click", function () {playSample(link[8]);});
+document.querySelector(".knopf0").addEventListener("click", function () {playSample(link[0])});
+document.querySelector(".knopf1").addEventListener("click", function () {playSample(link[1])});
+document.querySelector(".knopf2").addEventListener("click", function () {playSample(link[2])});
+document.querySelector(".knopf3").addEventListener("click", function () {playSample(link[3])});
+document.querySelector(".knopf4").addEventListener("click", function () {playSample(link[4])});
+document.querySelector(".knopf5").addEventListener("click", function () {playSample(link[5])});
+document.querySelector(".knopf6").addEventListener("click", function () {playSample(link[6])});
+document.querySelector(".knopf7").addEventListener("click", function () {playSample(link[7])});
+document.querySelector(".knopf8").addEventListener("click", function () {playSample(link[8])});
 
 //7.2 Weitergabe von Array Variabel an Main Funktion beim Click auf Knopf
 document.querySelector(".knopf9").addEventListener("click", playBeat)
